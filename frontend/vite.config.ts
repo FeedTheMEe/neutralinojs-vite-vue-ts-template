ee677a379f356f1ceb6ecda3430426a8043a19dc
FeedTheMEe <feedthe.mee@gmail.com>
2022-06-02 21:17:37 +0200

    first commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 8080
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src')
+    }
+  }
+})
