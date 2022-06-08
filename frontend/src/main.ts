c0c5aa7859d6192e8dada7715995663e2825d523
FeedTheMEe <feedthe.mee@gmail.com>
2022-06-08 18:22:33 +0200

    Async fix in main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,9 +4,11 @@ import App from './App.vue'
 createApp(App).mount('#app')
 
 if (!window.NL_PORT) {
-    const config = await import('./auth_info.json') // Make sure you successfully created a symlink for the auth_info (step 5 in README)
-    window.NL_PORT = config.port
-    window.NL_TOKEN = config.accessToken
+    (async () => {
+        const config = await import('./auth_info.json') // Make sure you successfully created a symlink for the auth_info (step 5 in README)
+        window.NL_PORT = config.port
+        window.NL_TOKEN = config.accessToken
+    })()
 }
 
 Neutralino.init()
