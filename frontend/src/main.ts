d86914429bac215613b4103291e966a5c743d7b7
FeedTheMEe <feedthe.mee@gmail.com>
2022-06-08 18:35:58 +0200

    Another async fix

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,7 @@ import App from './App.vue'
 createApp(App).mount('#app')
 
 if (!window.NL_PORT) {
-    (async () => {
+    await (async () => {
         const config = await import('./auth_info.json') // Make sure you successfully created a symlink for the auth_info (step 6 in README)
         window.NL_PORT = config.port
         window.NL_TOKEN = config.accessToken
