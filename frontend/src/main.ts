5ec1a15f9ee820fbca77384a29bbb358d6936721
FeedTheMEe <feedthe.mee@gmail.com>
2022-07-13 16:49:57 +0200

    fix

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,12 +3,12 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
+;(async () => {
 if (!window.NL_PORT) {
-    await (async () => {
-        const config = await import('./auth_info.json') // Make sure you successfully created a symlink for the auth_info (step 6 in README)
-        window.NL_PORT = config.port
-        window.NL_TOKEN = config.accessToken
-    })()
+    const config = await import('./auth_info.json') // Make sure you successfully created a symlink for the auth_info (step 6 in README)
+    window.NL_PORT = config.port
+    window.NL_TOKEN = config.accessToken
 }
 
 Neutralino.init()
+})()
